a1be02e3ad137111712a7e6d583e235b18c19cae
EC2 Default User <ec2-user@ip-172-31-25-148.us-west-2.compute.internal>
2021-09-04 19:12:20 +0000

    Docker Upload successful

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=abdocer123/mlapp:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag mlapp:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
